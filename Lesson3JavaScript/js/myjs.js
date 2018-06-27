59bab835e62b7dc72bd3c0f496528d01c0ae35ba
at-hungtran <hung.tran@asiantech.vn>
2018-06-27 08:56:30 +0700

    Fix convention js

--- Lesson3JavaScript/js/myjs.js
+++ Lesson3JavaScript/js/myjs.js
@@ -1,46 +1,41 @@
 function computeTheSum(numberOne, numberTwo) {
-  var sum=numberOne+numberTwo;
+  var sum = numberOne + numberTwo;
 
-  if(numberOne===numberTwo)
-    return sum*3;
+  if(numberOne === numberTwo)
+    return sum * 3;
   return sum;
 }
 
 function computeTheAbsolute(numInput) {  
   var numOutPut;
 
-  if(numInput<=19)
-    numOutPut=19-numInput;
+  if(numInput <= 19)
+    numOutPut = 19 - numInput;
   else 
-    numOutPut=(numInput-19)*3;
-
+    numOutPut = (numInput - 19) * 3;
   return numOutPut; 
 }
 
 function findMaskedNumber(input) {
-  var output=[];
+  var output = [];
 
-  for(var i=0; i<=9; i++) {
+  for(var i = 0; i <= 9; i++) {
     var inputAfterReplace=input.replace('*', i);
-
-    if(parseInt(inputAfterReplace)%3===0)
+    if(parseInt(inputAfterReplace) % 3 === 0)
       output.push(inputAfterReplace);
   }
-
   return output;
 }
 
 function findMaskedNumber(input) {
-  var output=[];
+  var output = [];
 
-  for(var i=0; i<=9; i++) {
+  for(var i = 0; i <= 9; i++) {
     var inputAfterReplace=input.replace('*', i);
-  
-    if(parseInt(inputAfterReplace)%6===0) {
+    if(parseInt(inputAfterReplace) % 6 === 0) {
       output.push(inputAfterReplace);
     }
   }
-
   return output;
 }
 
